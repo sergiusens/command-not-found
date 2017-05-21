95bcb9eedcf4b77d382c1f8eadec5e18807d65fb
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2017-05-21 18:57:42 -0300

    loop over DBs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 extern crate clap;
 use clap::{Arg, App};
 
+extern crate glob;
+use glob::glob;
+
 extern crate gdbm;
 use gdbm::{Gdbm};
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
+
+fn find_package(db_file_path: PathBuf, command: &str) {
+    let db = Gdbm::new(db_file_path.as_path(), 0, gdbm::READER, 0).unwrap();
+    if let Ok(data) = db.fetch(command) {
+      println!("The program '{}' is currently not installed. You can install it by typing:\nsudo apt install {}", command, data);
+    }
+}
 
 fn main() {
     let matches = App::new("command-not-found")
@@ -25,10 +35,13 @@ fn main() {
                                .index(1))
                   .get_matches();
 
+    let command = matches.value_of("command").unwrap();
     let data_dir = Path::new(matches.value_of("data_dir").unwrap_or("/usr/share/command-not-found"));
-    let db_file_path = data_dir.join("programs.d").join("all-main.db");
-    let db = Gdbm::new(db_file_path.as_path(), 0, gdbm::READER, 0).unwrap();
-    let data = db.fetch(matches.value_of("command").unwrap()).unwrap();
-  
-    println!("Please install: {}", data);
+    let db_files_path_match = data_dir.join("programs.d").join("*.db");
+    for db_path in glob(db_files_path_match.to_str().unwrap()).expect("Expected glob") {
+      match db_path {
+        Ok(path) => find_package(path, command),
+        Err(e) => println!("{:?}", e),
+      }
+    }
 }
