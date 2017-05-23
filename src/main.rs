0972ea534baeffc73f61f2ce605ec5dd0655f7af
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2017-05-23 10:33:07 -0300

    iterate over results and exit properly

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,42 @@ use glob::glob;
 extern crate gdbm;
 use gdbm::{Gdbm};
 
-use std::path::{Path, PathBuf};
+use std::process::exit;
+use std::path::{Path};
 
-fn find_package(db_file_path: PathBuf, command: &str) {
-    let db = Gdbm::new(db_file_path.as_path(), 0, gdbm::READER, 0).unwrap();
-    if let Ok(data) = db.fetch(command) {
-      println!("The program '{}' is currently not installed. You can install it by typing:\nsudo apt install {}", command, data);
+
+fn find_package(db_dir: &Path, command: &str) -> Result<Vec<String>, String> {
+    let db_files_path_match = db_dir.join("programs.d").join("*.db");
+    let mut packages: Vec<String> = Vec::new();
+
+    for db_path in glob(db_files_path_match.to_str().unwrap()).expect("Expected glob") {
+      match db_path {
+        Ok(path) => {
+          if let Ok(query) = query_database(path.as_path(), command) {
+            packages.push(query); 
+          }
+        }
+        Err(e) => println!("{:?}", e),
+      }
     }
+    if packages.is_empty() {
+      return Err("The were no packages found.".to_string())
+    }
+    return Ok(packages);
+}
+
+fn query_database(db_file_path: &Path, command: &str) -> Result<String, bool> {
+    let db = Gdbm::new(db_file_path, 0, gdbm::READER, 0).unwrap();
+    match db.fetch(command) {
+      Ok(data) => return Ok(data),
+      Err(_) => return Err(false), // handle error enum
+    }
+}
+
+fn print_packages(command: &str, packages_found: Vec<String>) {
+  for package_name in &packages_found {
+    println!("The program '{}' is currently not installed. You can install it by typing:\nsudo apt install {}", command, package_name);
+  }
 }
 
 fn main() {
@@ -37,11 +66,13 @@ fn main() {
 
     let command = matches.value_of("command").unwrap();
     let data_dir = Path::new(matches.value_of("data_dir").unwrap_or("/usr/share/command-not-found"));
-    let db_files_path_match = data_dir.join("programs.d").join("*.db");
-    for db_path in glob(db_files_path_match.to_str().unwrap()).expect("Expected glob") {
-      match db_path {
-        Ok(path) => find_package(path, command),
-        Err(e) => println!("{:?}", e),
+    
+    let packages = find_package(data_dir, command);
+    match packages {
+      Ok(packages_found) => print_packages(command, packages_found),
+      Err(e) => {
+        println!("{:?}", e);
+        exit(1);
       }
     }
 }
